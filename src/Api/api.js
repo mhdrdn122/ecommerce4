4920352772a114ddf806def85dd32e192139ec0d
mhdrdn122 <mhdrdn122@gmail.com>
2025-01-25 19:49:48 +0300

    add api link

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,6 @@
-export const BASEURL = "http://127.0.0.1:8000/api"
+// export const BASEURL = "http://127.0.0.1:8000/api"
+export const BASEURL = "https://backend-ecomerce4-production.up.railway.app/api"
+
 export const REGISTER = "register"
 export const LOGIN = "login"
 export const LOGOUT = "logout"
