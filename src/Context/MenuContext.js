3c34bed47af5f1e86b58a932d8b23cc5e7654893
mhdrdn122 <mhdrdn122@gmail.com>
2025-01-27 13:04:48 +0300

    add page 404 and update side bar

--- src/Context/MenuContext.js
+++ src/Context/MenuContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const Menu = createContext(true)
 
 const MenuContex = ({children}) => {
-    const [isOpen , setIsOpen] = useState(true)
+    const [isOpen , setIsOpen] = useState(false)
 
     return <Menu.Provider  value={{isOpen , setIsOpen}}>
     {children}
